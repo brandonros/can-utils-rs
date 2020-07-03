45ac26c7dfd7c9f71d1b62cff328d7a65dffa6d2
Brandon Ros <brandonros1@gmail.com>
2020-07-02 21:44:47 -0400

    get rid of threads

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 extern crate tungstenite;
 
-use std::sync::{Arc, Mutex};
 use can_utils_rs::{devices};
 
 /*
@@ -31,33 +30,29 @@ fn main() {
     // init server
     let server = std::net::TcpListener::bind("127.0.0.1:9001").unwrap();
     // listen for connections
-    let websockets = Arc::new(Mutex::new(vec![]));
+    let websockets = vec![];
     for stream in server.incoming() {
         let mut websocket = tungstenite::server::accept(stream.unwrap()).unwrap();
-        websockets.lock().unwrap().push(websocket);
+        websockets.push(websocket);
         // read from socket, send to device
-        std::thread::spawn (move || {
-            loop {
-                let msg = websocket.read_message().unwrap().into_data();
-                let arbitration_id = u32::from_be_bytes([
-                    msg[0],
-                    msg[1],
-                    msg[2],
-                    msg[3]
-                ]);
-                let data = &msg[4..];
-                devices::tactrix_openport::send_can_frame(&device_handle, arbitration_id, data);
-            }
-        });
+        loop {
+            let msg = websocket.read_message().unwrap().into_data();
+            let arbitration_id = u32::from_be_bytes([
+                msg[0],
+                msg[1],
+                msg[2],
+                msg[3]
+            ]);
+            let data = &msg[4..];
+            devices::tactrix_openport::send_can_frame(&device_handle, arbitration_id, data);
+        }
     }
     // read from device, send to sockets
-    std::thread::spawn (move || {
-        let mut handler = move |frame: Vec<u8>| {
-            for websocket in &mut *websockets.lock().unwrap() {
-                let binary_frame = tungstenite::Message::Binary(frame.clone());
-                websocket.write_message(binary_frame).unwrap();
-            }
-        };
-        devices::tactrix_openport::recv(&device_handle, &mut handler);
-    });
+    let handler = move |frame: Vec<u8>| {
+        for websocket in websockets {
+            let binary_frame = tungstenite::Message::Binary(frame.clone());
+            websocket.write_message(binary_frame).unwrap();
+        }
+    };
+    devices::tactrix_openport::recv(&device_handle, &handler);
 }
