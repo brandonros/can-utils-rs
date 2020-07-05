2175040c2d33ad1da6debdf0a010f63c90121c3f
Brandon Ros <brandonros1@gmail.com>
2020-07-05 00:03:32 -0400

    read write

--- src/server.rs
+++ src/server.rs
@@ -15,17 +15,19 @@ fn main() {
     let device_handle_arc = Arc::new(device_handle);
     // init server
     let server = std::net::TcpListener::bind("127.0.0.1:9001").unwrap();
-    let streams_map: HashMap<SocketAddr, TcpStream> = HashMap::new();
-    let streams_map_arc = Arc::new(Mutex::new(streams_map));
+    let read_streams_map: HashMap<SocketAddr, TcpStream> = HashMap::new();
+    let write_streams_map: HashMap<SocketAddr, TcpStream> = HashMap::new();
+    let read_streams_map_arc = Arc::new(Mutex::new(read_streams_map));
+    let write_streams_map_arc = Arc::new(Mutex::new(write_streams_map));
     // read from device, send to sockets
-    let streams_map_ref = streams_map_arc.clone();
+    let write_streams_map_ref = write_streams_map_arc.clone();
     let device_handle_ref = device_handle_arc.clone();
     let device_thread = std::thread::spawn(move || {
         let mut handler = move |frame: Vec<u8>| {
             println!("got frame = {:?}", frame);
-            let mut streams_map = streams_map_ref.lock().unwrap();
+            let mut write_streams_map = write_streams_map_ref.lock().unwrap();
             println!("unlocked");
-            for (peer_addr, stream) in streams_map.iter_mut() {
+            for (peer_addr, stream) in write_streams_map.iter_mut() {
                 let stream = stream.try_clone().unwrap();
                 let mut websocket = tungstenite::WebSocket::<std::net::TcpStream>::from_raw_socket(stream, Role::Client, None);
                 println!("writing to {:?}", peer_addr);
@@ -38,20 +40,22 @@ fn main() {
         devices::tactrix_openport::recv(&device_handle_ref, &mut handler);
     });
     // listen for connections
-    let _device_handle_ref = device_handle_arc.clone();
-    let streams_map_ref = streams_map_arc.clone();
+    let read_streams_map_ref = read_streams_map_arc.clone();
+    let write_streams_map_ref = write_streams_map_arc.clone();
     let server_thread = std::thread::spawn(move || {
         for stream in server.incoming() {
             let mut websocket = tungstenite::server::accept(stream.unwrap()).unwrap();
             let peer_addr = websocket.get_mut().peer_addr().unwrap();
-            let mut streams_map = streams_map_ref.lock().unwrap();
-            streams_map.insert(peer_addr, websocket.get_mut().try_clone().unwrap());
-            let streams_map_ref = streams_map_arc.clone();
+            let mut read_streams_map = read_streams_map_ref.lock().unwrap();
+            read_streams_map.insert(peer_addr, websocket.get_mut().try_clone().unwrap());
+            let mut write_streams_map = write_streams_map_ref.lock().unwrap();
+            write_streams_map.insert(peer_addr, websocket.get_mut().try_clone().unwrap());
+            let read_streams_map_ref = read_streams_map_arc.clone();
             let device_handle_ref = device_handle_arc.clone();
             // read from socket, send to device
             std::thread::spawn(move || {
-                let mut streams_map = streams_map_ref.lock().unwrap();
-                let stream = streams_map.get(&peer_addr).unwrap().try_clone().unwrap();
+                let mut read_streams_map = read_streams_map_ref.lock().unwrap();
+                let stream = read_streams_map.get(&peer_addr).unwrap().try_clone().unwrap();
                 let mut websocket = tungstenite::WebSocket::<std::net::TcpStream>::from_raw_socket(stream, Role::Client, None);
                 loop {
                     let msg = websocket
